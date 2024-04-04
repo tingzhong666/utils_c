373f7b5a121ce932fe1f453811c770923af2b94e
tingzhong <tingzhong666@gmail.com>
2024-04-04 16:27:56 +0800

    .

---
+++ utils.c
@@ -0,0 +1,12 @@
+/**
+ * utils.c
+ * @tingzhong666
+ * 2024-04-04 16:24:24
+ */
+#include <utils.h>
+
+// 打印当前位置
+void info()
+{
+    printf("err file:%s line:%d\n", __FILE__, __LINE__);
+}
---
+++ utils.h
@@ -0,0 +1,30 @@
+/**
+ * utils.h
+ * @tingzhong666
+ * 2024-04-04 16:24:38
+ */
+
+#ifndef UTILS_H
+#define UTILS_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include "stdio.h"
+
+    typedef struct
+    {
+        void *data;
+        int err;
+        char *err_msg;
+    } RES;
+
+    void info(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
